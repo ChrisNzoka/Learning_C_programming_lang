c4115c3482a08be61ac2fcb8761225bbb728b4e1
Chris Nzoka-okoye <chrisnzokaokoye@gmail.com>
2022-03-14 14:02:39 +0100

    Minimum width specifiers

--- getc_practice.c
+++ getc_practice.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /*
+*   getc(stdin) vs getchar()
 *   program reads character entered by the user
 *   and prints it to the screen
 */
 
 int main(void)
 {
-    printf("Enter a character from the keyboard: ");
+    printf("Enter 2 character from the keyboard: ");
     char a1 = getc(stdin);
-    printf("The character you entered is: %c\n.", a1);
+    char a2 = getchar();
+    printf("The characters you entered are: %c%c\n.", a1, a2);
     return (0);
 }
