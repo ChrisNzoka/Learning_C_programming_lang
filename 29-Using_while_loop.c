607efa57819cc3991eec75345c6811906de901b9
Chris Nzoka-okoye <chrisnzokaokoye@gmail.com>
2022-03-17 21:11:15 +0100

    Learning iteration in C

---
+++ 29-Using_while_loop.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - starting point
+*
+* Description - program prints character input
+* while the character is not equal to 'x'
+*
+* Return 0 on success
+*/
+
+int main(void)
+{
+  int c;
+
+  c = ' ';
+  puts("Enter a character: \n(enter x to exit)\n");
+  while (c != 'x')
+  {
+    c = getc(stdin);
+    putchar(c);
+  }
+  printf("\nOut of the while loop. Have a nice day!!!\n");
+  return (0);
+}
